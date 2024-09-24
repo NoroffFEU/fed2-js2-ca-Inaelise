e433af93e2fe8004f00cc61fe234501c93eca8cd
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-09-24 13:34:44 +0200

    Adds JS docs to login functions

--- src/js/api/auth/login.js
+++ src/js/api/auth/login.js
@@ -1,5 +1,15 @@
 import { API_AUTH_LOGIN } from "../constants";
 
+/**
+ * This will login a user by sending the email and password to the authentication API
+ * If successful, it stores the access token and user data in local storage
+ * @param {string} email This is the user's email
+ * @param {string} password This is the user's password
+ * @returns {object} The login data (access token and user) is returned
+ * @example
+ * const user = await login({ email: "example@stud.noroff.no", password: "example123" });
+ * console.log(user);
+ */
 export async function login({ email, password }) {
   const response = await fetch(API_AUTH_LOGIN, {
     headers: {
--- src/js/ui/auth/login.js
+++ src/js/ui/auth/login.js
@@ -1,5 +1,10 @@
 import { login } from "../../api/auth/login";
 
+/**
+ * This takes the entries from the login form and attempts to login a user
+ * @param {*} event logs in user on submit
+ * @returns {string} if successful an alert message pops up and the user is redirected to the home page, if unsuccessful an error message pops up
+ */
 export async function onLogin(event) {
   event.preventDefault();
   const loginForm = event.target;
