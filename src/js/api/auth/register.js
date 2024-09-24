d2a1458976aee9e9699f03b6f08e4a28ac0e7180
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-09-24 13:03:04 +0200

    Adds JSdocs to register functions

--- src/js/api/auth/register.js
+++ src/js/api/auth/register.js
@@ -1,5 +1,15 @@
 import { API_AUTH_REGISTER } from "../constants";
 
+/**
+ * This will register a user account in Noroff's API
+ * @param {string} name This is the name of the user
+ * @param {string} email This is the user's email
+ * @param {string} password This is the user's password
+ * @param {string} bio This is the user's bio
+ * @param {string} banner This is the user's banner
+ * @param {string} avatar This is the user's avatar
+ * @returns {object} The register data is returned
+ */
 export async function register({ name, email, password, bio, banner, avatar }) {
   const response = await fetch(API_AUTH_REGISTER, {
     headers: {
