52f6e5d0ff50b43a51f0ffb270b3b6d94f515d1f
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-09-24 10:24:02 +0200

    Adds register data from form and register function

--- src/js/ui/auth/register.js
+++ src/js/ui/auth/register.js
@@ -1,1 +1,16 @@
-export async function onRegister(event) {}
+import { register } from "../../api/auth/register";
+
+export async function onRegister(event) {
+  event.preventDefault();
+  const registerForm = event.target;
+  const formData = new FormData(registerForm);
+  const data = Object.fromEntries(formData.entries());
+
+  try {
+    await register(data);
+    alert("Thank you for registering!");
+    window.location.href = "/auth/login/";
+  } catch (error) {
+    alert(error);
+  }
+}
