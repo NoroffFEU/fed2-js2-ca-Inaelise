acb1a74c289084e6826b4bc0ec303a389e253518
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-09-24 10:24:54 +0200

    Adds login data from form and login function

--- src/js/api/auth/login.js
+++ src/js/api/auth/login.js
@@ -1,1 +1,21 @@
-export async function login({ email, password }) {}
+import { API_AUTH_LOGIN } from "../constants";
+
+export async function login({ email, password }) {
+  const response = await fetch(API_AUTH_LOGIN, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "post",
+    body: JSON.stringify({ email, password }),
+  });
+
+  if (response.ok) {
+    const { data } = await response.json();
+    const { accessToken: token, ...user } = data;
+    localStorage.token = token;
+    localStorage.user = JSON.stringify(user);
+    return data;
+  }
+
+  throw new Error("Could not login with this account");
+}
