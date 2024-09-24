ba8536a16a03f5a9d5b26a32acb10445aee7e43e
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-09-24 10:26:23 +0200

    Adds event listener and logout function to logout btn

--- src/js/ui/global/logout.js
+++ src/js/ui/global/logout.js
@@ -1,1 +1,13 @@
-export function setLogoutListener() {}
+import { onLogout } from "../auth/logout";
+
+export function setLogoutListener() {
+  const logoutBtn = document.querySelector(".logout-btn");
+
+  logoutBtn.addEventListener("click", () => {
+    if (window.confirm("Are you sure you want to logout?") === true) {
+      onLogout();
+      alert("You've logged out!");
+      window.location.href = "/auth/login/";
+    }
+  });
+}
