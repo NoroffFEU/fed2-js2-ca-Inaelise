9236960954d86caf2903ffdfcc8ef5b7e4459390
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-09-24 13:44:55 +0200

    Adds JS docs to logout functions

--- src/js/ui/auth/logout.js
+++ src/js/ui/auth/logout.js
@@ -1,3 +1,10 @@
+/**
+ * This will remove the access token and user data in local storage.
+ * It is a helper function for the function setLogoutListener()
+ *  @example
+ * // Call the logout function to remove the data:
+ * onLogout();
+ */
 export function onLogout() {
   localStorage.removeItem("token");
   localStorage.removeItem("user");
--- src/js/ui/global/logout.js
+++ src/js/ui/global/logout.js
@@ -1,5 +1,12 @@
 import { onLogout } from "../auth/logout";
 
+/**
+ * This will logout the user when the button is clicked.
+ * If confirmed, the user will logout. An alert message will pop up and the user will be redirected to the login page.
+ * @example
+ * // Call this function to set up the logout listener:
+ * setLogoutListener();
+ */
 export function setLogoutListener() {
   const logoutBtn = document.querySelector(".logout-btn");
 
