1a4662909c4bab1b35f5fe5baacc3b6652df13b2
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-09-24 10:22:19 +0200

    Adds register function

--- src/js/api/auth/register.js
+++ src/js/api/auth/register.js
@@ -1,8 +1,18 @@
-export async function register({
-  name,
-  email,
-  password,
-  bio,
-  banner,
-  avatar,
-}) {}
+import { API_AUTH_REGISTER } from "../constants";
+
+export async function register({ name, email, password, bio, banner, avatar }) {
+  const response = await fetch(API_AUTH_REGISTER, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "post",
+    body: JSON.stringify({ name, email, password, bio, banner, avatar }),
+  });
+
+  if (response.ok) {
+    const { data } = await response.json();
+    return data;
+  }
+
+  throw new Error("Could not register this account");
+}
