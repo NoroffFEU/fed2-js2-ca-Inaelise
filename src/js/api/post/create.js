7db8dc41817fa4072b9b6f3e5d90bbf108f92256
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-09-25 17:44:53 +0200

    Adds function for creating post to the api

--- src/js/api/post/create.js
+++ src/js/api/post/create.js
@@ -1,1 +1,25 @@
-export async function createPost({ title, body, tags, media }) {}
+import { API_SOCIAL_POSTS } from "../constants";
+import { headers as getHeaders } from "../headers";
+
+export async function createPost({ title, tags, media }) {
+  const token = localStorage.getItem("token");
+  if (!token) {
+    throw new Error("No authorization token found. Please log in.");
+  }
+
+  const requestHeaders = getHeaders(token);
+
+  const response = await fetch(API_SOCIAL_POSTS, {
+    headers: requestHeaders,
+    method: "POST",
+    body: JSON.stringify({ title, tags, media }),
+  });
+
+  if (response.ok) {
+    const { data } = await response.json();
+    alert("post created!");
+    return data;
+  }
+
+  throw new Error("Could not create post");
+}
