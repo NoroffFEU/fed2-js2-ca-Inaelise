dee59874f169f68e8fc56605931d01b96b7f8c4a
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-09-25 17:46:46 +0200

    Adds function to get form entries for creating new post

--- src/js/ui/post/create.js
+++ src/js/ui/post/create.js
@@ -1,1 +1,28 @@
-export async function onCreatePost(event) {}
+import { createPost } from "../../api/post/create";
+
+export async function onCreatePost(event) {
+  event.preventDefault();
+
+  const createForm = event.target;
+  const formData = new FormData(createForm);
+  const data = Object.fromEntries(formData.entries());
+
+  const media = {
+    url: data.url,
+    alt: "",
+  };
+
+  delete data.url;
+  data.media = media;
+
+  data.tags = data.tags.split(",").map((tag) => tag.trim());
+
+  console.log(data);
+
+  try {
+    const newPost = await createPost(data);
+    window.location.href = `/post/?id=${newPost.id}`;
+  } catch (error) {
+    alert("Error creating post: " + error.message);
+  }
+}
