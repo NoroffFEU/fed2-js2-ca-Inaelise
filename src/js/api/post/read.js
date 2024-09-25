2a436d672773eb980e74b91be99fa5fdbbd55476
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-09-25 17:50:55 +0200

    Adds function to get post with id

--- src/js/api/post/read.js
+++ src/js/api/post/read.js
@@ -1,4 +1,24 @@
-export async function readPost(id) {}
+import { API_SOCIAL_POSTS } from "../constants";
+import { headers as getHeaders } from "../headers";
+
+export async function readPost(id) {
+  const url = `${API_SOCIAL_POSTS}/${id}`;
+
+  const token = localStorage.getItem("token");
+
+  const requestHeaders = getHeaders(token);
+
+  const response = await fetch(url, {
+    headers: requestHeaders,
+  });
+
+  if (response.ok) {
+    const { data } = await response.json();
+    console.log(data);
+    return data;
+  }
+  throw new Error(`Could not fetch post with id: ${id}`);
+}
 
 export async function readPosts(limit = 12, page = 1, tag) {}
 
