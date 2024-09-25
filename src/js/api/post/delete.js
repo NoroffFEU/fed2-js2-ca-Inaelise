55625b87c6295360580265d8668f446f31aaafae
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-09-25 17:53:45 +0200

    Adds function to delete post from api

--- src/js/api/post/delete.js
+++ src/js/api/post/delete.js
@@ -1,1 +1,22 @@
-export async function deletePost(id) {}
+import { API_SOCIAL_POSTS } from "../constants";
+import { headers as getHeaders } from "../headers";
+
+export async function deletePost(id) {
+  const url = `${API_SOCIAL_POSTS}/${id}`;
+
+  const token = localStorage.getItem("token");
+
+  const requestHeaders = getHeaders(token);
+
+  const response = await fetch(url, {
+    headers: requestHeaders,
+    method: "DELETE",
+  });
+
+  if (response.ok) {
+    alert("Post deleted!");
+    return true;
+  }
+
+  throw new Error("Could not delete post");
+}
