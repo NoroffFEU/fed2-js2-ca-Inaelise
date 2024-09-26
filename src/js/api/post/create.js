99838be0a6caec39b596d83a681369c6aaded02b
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-09-26 09:45:47 +0200

    Adds body param

--- src/js/api/post/create.js
+++ src/js/api/post/create.js
@@ -4,17 +4,19 @@ import { headers as getHeaders } from "../headers";
 /**
  * This function will create a new post by sending a POST request to the API.
  * @param {string} title This is the title of the post
+ * @param {string} body This is the body of the post
  * @param {string} tags This is an array of tags for the post
  * @param {object} media This is the media object for the post
  * @returns {object} The data for the created post is returned
  * @example
  * const newPost = {
  *  title: "New post"
+ *  body: "This is a new post",
  *  tags: ["new", "tech"],
  *  media: { url:"https://example.com/image.jpg", alt: "An example image" }
  * };
  */
-export async function createPost({ title, tags, media }) {
+export async function createPost({ title, body, tags, media }) {
   const token = localStorage.getItem("token");
   if (!token) {
     throw new Error("No authorization token found. Please log in.");
@@ -25,7 +27,7 @@ export async function createPost({ title, tags, media }) {
   const response = await fetch(API_SOCIAL_POSTS, {
     headers: requestHeaders,
     method: "POST",
-    body: JSON.stringify({ title, tags, media }),
+    body: JSON.stringify({ title, body, tags, media }),
   });
 
   if (response.ok) {
