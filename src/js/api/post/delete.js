ee6b086c2c3c89d3b7c8d5c124fcd974262698f4
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-09-26 11:49:39 +0200

    Adds documentation

--- src/js/api/post/delete.js
+++ src/js/api/post/delete.js
@@ -1,6 +1,11 @@
 import { API_SOCIAL_POSTS } from "../constants";
 import { headers as getHeaders } from "../headers";
 
+/**
+ * This function will delete a post by its ID.
+ * @param {number} id the id of the post.
+ * @returns if successful an alert pops up and it returns true. If unsuccessful an error is thrown.
+ */
 export async function deletePost(id) {
   const url = `${API_SOCIAL_POSTS}/${id}`;
 
