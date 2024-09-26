439ea9393b1034b185dcbe40141b9838c659e3f6
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-09-26 14:49:35 +0200

    Adds functions for handling and updating post

--- src/js/api/post/update.js
+++ src/js/api/post/update.js
@@ -1,1 +1,23 @@
-export async function updatePost(id, { title, body, tags, media }) {}
+import { API_SOCIAL_POSTS } from "../constants";
+import { headers as getHeaders } from "../headers";
+
+export async function updatePost(id, { title, body, tags, media }) {
+  const url = `${API_SOCIAL_POSTS}/${id}`;
+  const token = localStorage.getItem("token");
+
+  const requestHeaders = getHeaders(token);
+
+  const response = await fetch(url, {
+    headers: requestHeaders,
+    method: "PUT",
+    body: JSON.stringify({ title, body, tags, media }),
+  });
+
+  if (response.ok) {
+    const { data } = await response.json();
+    alert("Post updated!");
+    return data;
+  }
+
+  throw new Error(`Could not update post with id: ${id}`);
+}
