ec7920616ba0346c45db1c03fc60af1e3fa45c57
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-09-27 12:17:04 +0200

    Adds function to update profile in api

--- src/js/api/profile/update.js
+++ src/js/api/profile/update.js
@@ -1,1 +1,23 @@
-export async function updateProfile(username, { avatar, banner }) {}
+import { API_SOCIAL_PROFILES } from "../constants";
+import { headers as getHeaders } from "../headers";
+
+export async function updateProfile(username, { avatar, bio }) {
+  const url = `${API_SOCIAL_PROFILES}/${username}`;
+  const token = localStorage.getItem("token");
+
+  const requestHeaders = getHeaders(token);
+
+  const response = await fetch(url, {
+    headers: requestHeaders,
+    method: "PUT",
+    body: JSON.stringify({ avatar, bio }),
+  });
+
+  if (response.ok) {
+    const { data } = await response.json();
+    alert("Profile updated!");
+    return data;
+  }
+
+  throw new Error(`Could not update profile with username: ${username}`);
+}
