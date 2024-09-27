252e386aeb14bb9e47ae0647985144e49ec9fec2
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-09-27 12:17:29 +0200

    Adds function to handle update form data

--- src/js/ui/profile/update.js
+++ src/js/ui/profile/update.js
@@ -1,1 +1,26 @@
-export async function onUpdateProfile(event) {}
+import { updateProfile } from "../../api/profile/update";
+import { activeUser } from "../../utilities/activeUser";
+
+export async function onUpdateProfile(event) {
+  event.preventDefault();
+  const user = activeUser();
+
+  const updateForm = event.target;
+  const formData = new FormData(updateForm);
+  const data = Object.fromEntries(formData.entries());
+
+  const media = {
+    url: data.url,
+    alt: "",
+  };
+
+  delete data.url;
+  data.media = media;
+
+  try {
+    await updateProfile(user.name, data);
+    window.location.href = "/profile/";
+  } catch (error) {
+    alert("Error updating profile: " + error.message);
+  }
+}
