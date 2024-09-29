24eac0c5c9a746f9bb485f16368776776e3b14e1
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-09-29 11:05:21 +0200

    Adds function to populate form inputs

--- src/js/utilities/populateForm.js
+++ src/js/utilities/populateForm.js
@@ -1,5 +1,7 @@
 import { readPost } from "../api/post/read";
+import { readProfile } from "../api/profile/read";
 import { activePostId } from "./activePostId";
+import { activeUser } from "./activeUser";
 
 /**
  * This function will populate the form fields with the existing data.
@@ -18,5 +20,20 @@ export async function populateEditForm() {
     form["tags"].value = post.tags ? post.tags.join(", ") : "";
   } catch (error) {
     alert("Error loading post data to input fields.");
+  }
+}
+
+export async function populateProfileForm() {
+  const user = activeUser();
+
+  try {
+    const profile = await readProfile(user.name);
+
+    const form = document.forms.updateProfile;
+
+    form["url"].value = profile.avatar?.url || "";
+    form["bio"].value = profile.bio || "";
+  } catch (error) {
+    alert("Error loading profile data to input fields.");
   }
 }
