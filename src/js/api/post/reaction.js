5d13f3550bc8d828de37764d04a80e2e3311ce07
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-09-29 11:08:51 +0200

    Removes console.log

--- src/js/api/post/reaction.js
+++ src/js/api/post/reaction.js
@@ -23,7 +23,6 @@ export async function likePost(postId, symbol = "👍") {
 
   if (!response.ok) {
     const result = await response.json();
-    console.log(result);
     const error = result.errors.map((error) => error.message).join("\r\n");
     throw new Error(error);
   }
