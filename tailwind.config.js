0bf0a4727083e30f96d62ffe7e5781cbfb4596cc
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-11-03 13:05:49 +0100

    Adds themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./**/*.{html,js,ts}", "!./node_modules/**/*"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#ACE3D6",
+      },
+      fontFamily: {
+        body: ["Inter", "sans-serif"],
+      },
+      fontSize: {
+        sm: "0.9rem",
+      },
+    },
   },
   plugins: [],
 };
