6c7eafb6bf71abea7e0fd645d9e2e7f8542a4356
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-11-03 13:07:36 +0100

    Adds dropdown menu function

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -1,8 +1,10 @@
 import { setLogoutListener } from "../../ui/global/logout";
 import { authGuard } from "../../utilities/authGuard";
 import { viewPosts, setupPagination } from "../../ui/post/viewPosts";
+import { toggleDropdown } from "../../utilities/dropdownMenu";
 
 authGuard();
 viewPosts();
 setupPagination();
 setLogoutListener();
+toggleDropdown();
