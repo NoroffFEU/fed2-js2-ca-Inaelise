fd745ff79a1e7e2e73bcae70b5111d05949c55f5
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-11-06 10:38:02 +0100

    Adds logout function

--- src/js/router/views/login.js
+++ src/js/router/views/login.js
@@ -1,8 +1,10 @@
 import { onLogin } from "../../ui/auth/login";
 import { toggleDropdown } from "../../utilities/dropdownMenu";
+import { setLogoutListener } from "../../ui/global/logout";
 
 const form = document.forms.login;
 
 form.addEventListener("submit", onLogin);
 
 toggleDropdown();
+setLogoutListener();
