85342ebec3bf97317ceacf0828819e9605ef7ab5
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-11-06 18:19:11 +0100

    Adds dropdown menu function

--- src/js/router/views/profile.js
+++ src/js/router/views/profile.js
@@ -1,6 +1,7 @@
 import { authGuard } from "../../utilities/authGuard";
 import { viewProfile } from "../../ui/profile/viewProfile";
+import { toggleDropdown } from "../../utilities/dropdownMenu";
 
 authGuard();
-
+toggleDropdown();
 viewProfile();
