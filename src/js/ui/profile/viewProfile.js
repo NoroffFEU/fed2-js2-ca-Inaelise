c33e599e0556000a10c8388c7c95af4c17faae28
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-11-06 18:17:21 +0100

    Adds style classes to profile elements

--- src/js/ui/profile/viewProfile.js
+++ src/js/ui/profile/viewProfile.js
@@ -14,10 +14,11 @@ export async function viewProfile() {
 
     const img = document.createElement("img");
     img.src = profile.avatar.url;
-    img.classList.add("rounded-full", "w-[200px]");
+    img.classList.add("rounded-full", "w-[200px]", "shadow-spread");
 
     const userName = document.createElement("h2");
     userName.textContent = profile.name;
+    userName.classList.add("text-2xl", "font-semibold");
 
     const bio = document.createElement("p");
     bio.textContent = profile.bio;
@@ -28,7 +29,8 @@ export async function viewProfile() {
       "gap-8",
       "text-center",
       "pt-8",
-      "pb-[50px]"
+      "pb-[50px]",
+      "uppercase"
     );
 
     const postCountDiv = document.createElement("div");
@@ -36,27 +38,48 @@ export async function viewProfile() {
 
     const postCount = document.createElement("p");
     postCount.textContent = profile._count.posts;
+    postCount.classList.add("text-2xl", "font-bold", "text-secondary");
 
     const postCountText = document.createElement("p");
     postCountText.textContent = "Posts";
+    postCountText.classList.add(
+      "text-xs",
+      "text-faded",
+      "font-black",
+      "md:text-sm"
+    );
 
     const followerCountDiv = document.createElement("div");
     followerCountDiv.classList.add("flex", "flex-col");
 
     const followerCount = document.createElement("p");
     followerCount.textContent = profile._count.followers;
+    followerCount.classList.add("text-2xl", "font-bold", "text-secondary");
 
     const followerCountText = document.createElement("p");
     followerCountText.textContent = "Followers";
+    followerCountText.classList.add(
+      "text-xs",
+      "text-faded",
+      "font-black",
+      "md:text-sm"
+    );
 
     const followingCountDiv = document.createElement("div");
     followingCountDiv.classList.add("flex", "flex-col");
 
     const followingCount = document.createElement("p");
     followingCount.textContent = profile._count.following;
+    followingCount.classList.add("text-2xl", "font-bold", "text-secondary");
 
     const followingCountText = document.createElement("p");
     followingCountText.textContent = "Following";
+    followingCountText.classList.add(
+      "text-xs",
+      "text-faded",
+      "font-black",
+      "md:text-sm"
+    );
 
     const editBtn = document.createElement("a");
     editBtn.href = "/profile/edit/";
