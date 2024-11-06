b2b327556ab84bc07e9acc0c5c5439fa238f240c
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-11-06 12:07:30 +0100

    Adds dropdown menu function

--- src/js/router/views/register.js
+++ src/js/router/views/register.js
@@ -1,5 +1,8 @@
 import { onRegister } from "../../ui/auth/register";
+import { toggleDropdown } from "../../utilities/dropdownMenu";
 
 const form = document.forms.register;
 
 form.addEventListener("submit", onRegister);
+
+toggleDropdown();
