8c914c8ec228588d8c920d8b75f6719f8cce11ec
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-11-06 10:03:49 +0100

    Adds dropdown function

--- src/js/router/views/login.js
+++ src/js/router/views/login.js
@@ -1,5 +1,8 @@
 import { onLogin } from "../../ui/auth/login";
+import { toggleDropdown } from "../../utilities/dropdownMenu";
 
 const form = document.forms.login;
 
 form.addEventListener("submit", onLogin);
+
+toggleDropdown();
