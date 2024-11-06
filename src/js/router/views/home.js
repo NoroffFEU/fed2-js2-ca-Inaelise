18c61f9233adde129b26a99474fd9b63bd314f4d
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-11-06 08:55:04 +0100

    Adds JS to redirect to login page when no token found

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -3,8 +3,14 @@ import { authGuard } from "../../utilities/authGuard";
 import { viewPosts, setupPagination } from "../../ui/post/viewPosts";
 import { toggleDropdown } from "../../utilities/dropdownMenu";
 
-authGuard();
-viewPosts();
-setupPagination();
-setLogoutListener();
-toggleDropdown();
+const token = localStorage.getItem("token");
+
+if (!token) {
+  window.location.href = "/auth/login/";
+} else {
+  authGuard();
+  viewPosts();
+  setupPagination();
+  setLogoutListener();
+  toggleDropdown();
+}
