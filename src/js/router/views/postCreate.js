6ba6cc49242a9f9cb3c53c22d3bbd11065bc37ad
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-11-09 09:56:12 +0100

    Adds dropdown menu function

--- src/js/router/views/postCreate.js
+++ src/js/router/views/postCreate.js
@@ -1,7 +1,9 @@
 import { onCreatePost } from "../../ui/post/create";
 import { authGuard } from "../../utilities/authGuard";
+import { toggleDropdown } from "../../utilities/dropdownMenu";
 
 authGuard();
+toggleDropdown();
 
 const form = document.forms.createPost;
 
